af67b10e548553186c6d19c17d86fa710862ff4c
Braden <braden.douglass@gmail.com>
2015-11-12 18:39:12 -0500

    Add In Required Items For New Planck Layouts
    Move old layout to that of the new plancks. This is mapped from what is
    done in the default layout file in the keymaps directory for the Planck
    keyboard.

--- keyboard/planck/keymaps/keymap_braden.c
+++ keyboard/planck/keymaps/keymap_braden.c
@@ -1,23 +1,27 @@
-#include "keymap_common.h"
+#include "planck.h"
+
+#define QWRT 0
+#define WORK 1
+#define MDIA 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = { /* Qwerty */
-    {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_ESC,  KC_BSPC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P},
+  [QWRT] = { /* Qwerty */
+    {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_ESC,  KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P},
     {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_TAB,  KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
     {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_ENT,  KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
     {KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, FUNC(1), FUNC(1), KC_SPC, KC_LGUI, KC_LALT, KC_LCTL, KC_RSFT}
   },
-  [1] = { /* Workman P */
-    {KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,   KC_ESC,  KC_BSPC,  KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN},
+  [WORK] = { /* Workman P */
+    {KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,   KC_ESC,  KC_BSPC, KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN},
     {KC_A,    KC_S,    KC_H,    KC_T,    KC_G,   KC_TAB,  KC_QUOT, KC_Y,    KC_N,    KC_E,    KC_O,    KC_I},
     {KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,   KC_ENT,  KC_ENT,  KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH},
     {KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, FUNC(1), FUNC(1), KC_SPC, KC_LGUI, KC_LALT, KC_LCTL, KC_RSFT}
     },
-  [2] = { /* Utils */
-    {FUNC(3),    KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,       KC_0,    FUNC(4)},
-    {KC_GRV,    S(KC_GRV), S(KC_9), S(KC_0), S(KC_MINS),  KC_MINS, KC_EQL, S(KC_EQL),  S(KC_LBRC), S(KC_RBRC), KC_LBRC, KC_RBRC},
-    {S(KC_GRV), S(KC_1),   S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8),    S(KC_COMM),    S(KC_DOT), KC_BSLS},
-    {KC_MS_L,   KC_MS_U,   KC_MS_D, KC_MS_R, KC_BTN1, KC_TRNS, KC_TRNS, KC_BTN2, KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS}
+  [MDIA] = { /* Utils */
+    {FUNC(3),   KC_1,      KC_2,    KC_3,    KC_4,       KC_5,    KC_6,    KC_7,      KC_8,       KC_9,       KC_0,      FUNC(4)},
+    {KC_GRV,    S(KC_GRV), S(KC_9), S(KC_0), S(KC_MINS), KC_MINS, KC_EQL,  S(KC_EQL), S(KC_LBRC), S(KC_RBRC), KC_LBRC,   KC_RBRC},
+    {S(KC_GRV), S(KC_1),   S(KC_2), S(KC_3), S(KC_4),    S(KC_5), S(KC_6), S(KC_7),   S(KC_8),    S(KC_COMM), S(KC_DOT), KC_BSLS},
+    {KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_L,    KC_MS_U,    KC_MS_D, KC_MS_R}
     },
 };
 
@@ -31,10 +35,15 @@ const uint16_t PROGMEM fn_actions[] = {
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
-    switch(id) {
-      case 0:
-        return MACRODOWN(T(CM_T), END);
-      break;
-    }
+      switch(id) {
+        case 0:
+          if (record->event.pressed) {
+            register_code(KC_RSFT);
+            backlight_step();
+          } else {
+            unregister_code(KC_RSFT);
+          }
+        break;
+      }
     return MACRO_NONE;
 };
