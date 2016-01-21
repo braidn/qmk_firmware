8e6e5b60dd49268451b3d49a9e5daf43d6029b8e
Braden <braden.douglass@gmail.com>
2016-01-21 08:37:20 -0500

    Update To Planck Bottom Row

--- keyboard/planck/keymaps/keymap_braden.c
+++ keyboard/planck/keymaps/keymap_braden.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {FUNC(3),   KC_1,      KC_2,    KC_3,    KC_4,       KC_5,    KC_6,    KC_7,      KC_8,       KC_9,       KC_0,      FUNC(4)},
     {KC_GRV,    S(KC_GRV), S(KC_9), S(KC_0), S(KC_MINS), KC_MINS, KC_EQL,  S(KC_EQL), S(KC_LBRC), S(KC_RBRC), KC_LBRC,   KC_RBRC},
     {S(KC_GRV), S(KC_1),   S(KC_2), S(KC_3), S(KC_4),    S(KC_5), S(KC_6), S(KC_7),   S(KC_8),    S(KC_COMM), S(KC_DOT), KC_BSLS},
-    {MT(MOD_LSFT, KC_LEFT), MT(MOD_LCTL, KC_DOWN), MT(MOD_LALT, KC_UP), MT(MOD_LGUI, KC_RIGHT), KC_BTN2, KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_L,    KC_MS_U,    KC_MS_D, KC_MS_R}
+    {SFT_T(KC_LEFT), CTL_T(KC_UP), ALT_T(KC_DOWN), GUI_T(KC_RIGHT), KC_PIPE, KC_TRNS, KC_TRNS, KC_PIPE, GUI_T(KC_LEFT), ALT_T(KC_UP), CTL_T(KC_DOWN), SFT_T(KC_RIGHT)}
     },
 };
 
