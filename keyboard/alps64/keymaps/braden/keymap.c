cacaa5d5abc8f3dc23125f2ef0b061ea8c4473e9
Braden <braden.douglass@gmail.com>
2016-08-13 18:59:16 -0400

    Update To Alps 60 Layout

--- keyboard/alps64/keymaps/braden/keymap.c
+++ keyboard/alps64/keymaps/braden/keymap.c
@@ -5,12 +5,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV, \
            FN5, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
            FN7, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
-           FN6, NUBS, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0, \
-           CAPS,LALT,LGUI,          SPC,                     RGUI, RALT,FN1,FN0),
+           FN6, NUBS, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN1, \
+           CAPS,LALT,FN9,          SPC,                     RGUI, FN8,FN0,FN0),
   [1] = \
     KEYMAP(TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS, FN4, \
            TRNS, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   TRNS,TRNS,DEL, \
-           TRNS, A,   S,   D,   F,   G,   LEFT,DOWN,UP,  RGHT,TRNS,TRNS,TRNS,  \
+           TRNS, VOLD,VOLU,D,   F,   G,   LEFT,DOWN,UP,  RGHT,TRNS,TRNS,TRNS,  \
            TRNS, TRNS,Z,   X,   C,   V,   B,   N,   M,   TRNS,TRNS,TRNS,TRNS,TRNS, \
            TRNS, TRNS,TRNS,          TRNS,                     TRNS, TRNS,FN3,TRNS),
   [2] = \
@@ -36,8 +36,10 @@ const uint16_t PROGMEM fn_actions[] = {
   [3] = ACTION_DEFAULT_LAYER_SET(0),
   [4] = ACTION_DEFAULT_LAYER_SET(2),
   [5] = ACTION_LAYER_TAP_KEY(1, KC_TAB),
-  [6] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),      // Backspace with tap LShift
-  [7] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC)
+  [6] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),  // Backspace with tap LShift
+  [7] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),   // Esc with tap caps lock/control
+  [8] = ACTION_MODS_TAP_KEY(MOD_RALT, KC_GRV),   // Grave with tap right alt
+  [9] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_ENT)    // Enter with tap left GUI
 };
 
 /*
