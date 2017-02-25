e553b5b75bb36c16fca9bf555e22366da7029f20
Braden <braden.douglass@gmail.com>
2017-02-25 13:36:11 -0500

    Update To WhiteFox Layout

--- keyboards/whitefox/keymaps/braden/keymap.c
+++ keyboards/whitefox/keymaps/braden/keymap.c
@@ -35,7 +35,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_FN5, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,     KC_DEL, \
         KC_FN7,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,      KC_PGUP, \
         KC_FN6,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP,  KC_PGDN, \
-        KC_CAPS,KC_LALT,KC_LGUI,               KC_SPC,           KC_RGUI,KC_RALT,KC_TRNS,     KC_LEFT,KC_DOWN,KC_RGHT \
+        KC_CAPS,KC_LALT,KC_FN8,               KC_SPC,           KC_RGUI,KC_RALT,KC_TRNS,     KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     [1] = KEYMAP( \
         KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, KC_F12, KC_TRNS,KC_TRNS, KC_FN4, \
