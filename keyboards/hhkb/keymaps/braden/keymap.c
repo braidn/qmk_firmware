da79196618a59eb7b01f12f0421cec0091c60859
Braden <braden.douglass@gmail.com>
2018-03-17 15:08:38 -0400

    HHKB Update.
    Throw together a quick numb pad like layer.

--- keyboards/hhkb/keymaps/braden/keymap.c
+++ keyboards/hhkb/keymaps/braden/keymap.c
@@ -1,28 +1,42 @@
 #include "hhkb.h"
 
 #define BASE 0
-#define ALT 1
-#define NORM 2
+#define NAVI 1
+#define NUMB 2
+#define NORM 3
+#define NAVN 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = \
     KEYMAP(KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV, \
            KC_FN4, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC, \
            KC_FN12,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,KC_FN6, \
-           KC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,KC_FN1, \
+           KC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,MO(2), \
                 KC_FN7,   KC_FN6,          KC_SPC,                KC_FN13,KC_FN11),
-    [ALT] = \
+    [NAVI] = \
     KEYMAP(KC_GRV,  KC_FN0, KC_FN2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_FN0, KC_FN2, \
-           KC_TRNS, KC_1,   KC_MS_U, KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_BTN1,  KC_BTN2,  KC_BSLS, \
+           KC_TRNS, KC_BTN1,KC_MS_U, KC_BTN2,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_BTN1,  KC_BTN2,  KC_BSLS, \
            KC_TRNS, KC_MS_L,KC_MS_D, KC_MS_R,KC_PGDN,KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_NO,  KC_BSLS,  KC_ENT, \
            KC_BSPC, KC_VOLD,KC_VOLU, KC_NO,  KC_NO,  KC_PGUP,  KC_HOME,KC_PGDN,KC_PGUP,KC_END, KC_NO, KC_BSPC,KC_TRNS, \
+                KC_TRNS,KC_TRNS,          KC_SPC,                KC_TRNS,KC_TRNS),
+    [NUMB] = \
+    KEYMAP(KC_GRV,  KC_FN0, KC_FN2,  KC_F3, KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_FN0, KC_FN2, \
+           KC_TRNS, KC_1,   KC_2,    KC_3,  S(KC_1),S(KC_2),S(KC_3),KC_7,   KC_8,   KC_9,   KC_0,  KC_BTN1,  KC_BTN2,  KC_BSLS, \
+           KC_TRNS, KC_4,   KC_5,    KC_6,  S(KC_4),S(KC_5),S(KC_6),KC_DOWN,KC_UP,  KC_RGHT,KC_NO,  KC_BSLS,  KC_ENT, \
+           KC_BSPC, KC_7,   KC_8,    KC_9,  S(KC_7),S(KC_8),S(KC_9),KC_PGDN,KC_PGUP,KC_END, KC_NO, KC_BSPC,KC_TRNS, \
                 KC_TRNS,KC_TRNS,          KC_SPC,                KC_TRNS,KC_TRNS),
     [NORM] = \
     KEYMAP(KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV, \
            KC_FN4, KC_Q,   KC_W,   KC_D,   KC_F,   KC_K,   KC_J,   KC_U,   KC_R,   KC_L,   KC_SCLN,   KC_LBRC,KC_RBRC,KC_BSPC, \
            KC_FN12,KC_A,   KC_S,   KC_E,   KC_T,   KC_G,   KC_Y,   KC_N,   KC_I,   KC_O,   KC_H, KC_QUOT,KC_FN6, \
-           KC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_P,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,KC_FN1, \
+           KC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_P,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,MO(4), \
                 KC_LALT,   KC_FN6,          KC_SPC,                KC_FN13, KC_FN11),
+    [NAVN] = \
+    KEYMAP(KC_GRV,  KC_FN0, KC_FN2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_FN0, KC_FN2, \
+           KC_TRNS, KC_BTN1,KC_MS_U, KC_BTN2,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_BTN1,  KC_BTN2,  KC_BSLS, \
+           KC_TRNS, KC_MS_L,KC_MS_D, KC_MS_R,KC_PGDN,KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_NO,  KC_BSLS,  KC_ENT, \
+           KC_BSPC, KC_VOLD,KC_VOLU, KC_NO,  KC_NO,  KC_PGUP,  KC_HOME,KC_PGDN,KC_PGUP,KC_END, KC_NO, KC_BSPC,KC_TRNS, \
+                KC_TRNS,KC_TRNS,          KC_SPC,                KC_TRNS,KC_TRNS),
 };
 
 /*
@@ -30,8 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_DEFAULT_LAYER_SET(0),                // Default layer(not used)
-    [1] = ACTION_LAYER_TOGGLE(1),                 // HHKB layer(toggle with 5 taps)
-    [2] = ACTION_DEFAULT_LAYER_SET(2),          // Vi layer with Slash*
+    [2] = ACTION_DEFAULT_LAYER_SET(3),          // Vi layer with Slash*
     [3] = ACTION_LAYER_TAP_KEY(3, KC_SCLN),           // Mousekey layer with Semicolon*
     [4] = ACTION_LAYER_TAP_KEY(1, KC_TAB),            // Mousekey layer with Space
     [5] = ACTION_LAYER_MOMENTARY(4),                  // Mousekey layer(IJKL)
