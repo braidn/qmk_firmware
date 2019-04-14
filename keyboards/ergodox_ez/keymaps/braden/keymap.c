3111a5969f595cbbebedf533e83eff07c11c82da
Braden Douglass <braden.douglass@gmail.com>
2019-04-13 22:32:22 -0400

    Update Ergodox (OLD)

--- keyboards/ergodox_ez/keymaps/braden/keymap.c
+++ keyboards/ergodox_ez/keymaps/braden/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_LCTL, // Single 1u thumb cluster
                                  KC_FN6, KC_ENT, TG(2), // Enter/Space/Lower 1u thumb cluster
         // right hand
-             KC_GESC, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_EQL,
+             KC_GESC, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,
              KC_RPRN,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,
              KC_RBRC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [MDIA] = LAYOUT_ergodox(  // Layer2: numpad, leftled:blue
         // left hand
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_EQL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_GRV,
         KC_TRNS, KC_MINS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_EQL,  KC_4, KC_5, KC_6, KC_TRNS,
         KC_TRNS, KC_0,    KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 KC_TRNS,
                               KC_TRNS, KC_BSPC, KC_TRNS,
         // right hand
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS,
              KC_TRNS, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PGUP,
                       KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS, KC_PGDN,
              KC_TRNS, KC_FN17, KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_PENT,
